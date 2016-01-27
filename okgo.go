e65ea967fc6e77801259e0fd4a5e0d2da4ae4737
Arseny Zarechnev <cyanidesign@gmail.com>
2016-01-27 20:58:43 +0000

    Bump version, it is becoming a thing already!

--- okgo.go
+++ okgo.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	app := cli.NewApp()
 	app.Name = "okgo"
-	app.Version = "0.0.1"
+	app.Version = "0.1.0"
 	app.Usage = "Scheduling service and API"
 	app.Action = cli.ShowAppHelp
 
